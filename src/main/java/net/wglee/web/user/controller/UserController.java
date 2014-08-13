ea878f698d56d7e8a637f5b439758297866606b9
Lee Wangeun <wglee21g@gmail.com>
2014-08-13 16:28:14 +0900

    Controller 구현

--- src/main/java/net/wglee/web/user/controller/UserController.java
+++ src/main/java/net/wglee/web/user/controller/UserController.java
@@ -1,7 +1,37 @@
 package net.wglee.web.user.controller;
 
+import net.wglee.user.model.User;
+import net.wglee.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
 /**
  * @Author wangeun.lee@sk.com
  */
+@Controller
+@RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginForm() {
+		return "user/login";
+	}
+
+	@RequestMapping(value = "/regist", method = RequestMethod.GET)
+	public String showRegistForm() {
+		return "user/regist";
+	}
+
+	@RequestMapping(value = "/regist", method = RequestMethod.POST)
+	public RedirectView regist(User user) {
+		userService.addUser(user);
+		RedirectView view = new RedirectView("/user/login");
+		view.setExposeModelAttributes(false);
+		return view;
+	}
 }
---
+++ src/test/java/net/wglee/web/user/controller/UserControllerTest.java
@@ -0,0 +1,61 @@
+package net.wglee.web.user.controller;
+
+import net.wglee.config.ApplicationConfig;
+import net.wglee.user.model.User;
+import net.wglee.user.service.UserService;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.servlet.view.RedirectView;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @Author wangeun.lee@sk.com
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ApplicationConfig.class)
+public class UserControllerTest {
+	@Autowired
+	@InjectMocks
+	private UserController controller;
+
+	@Mock
+	private UserService service;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testShowLoginForm() throws Exception {
+		String viewName = controller.showLoginForm();
+
+		assertEquals("user/login", viewName);
+	}
+
+	@Test
+	public void testShowRegistForm() throws Exception {
+		String viewName = controller.showRegistForm();
+
+		assertEquals("user/regist", viewName);
+	}
+
+	@Ignore
+	@Test
+	public void testRegist() throws Exception {
+		User user = new User();
+		user.setEmail("test@test.com");
+		user.setNickname("testNickName");
+		user.setPassword("12345678!");
+
+		RedirectView view = controller.regist(user);
+
+		assertNotNull(view);
+	}
+}
